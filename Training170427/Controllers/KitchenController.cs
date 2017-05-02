8e00f75b50fae8a0cf81b3495b84d287ac392cb0
ghozibahri <ghozibahri12@gmail.com>
2017-05-02 08:51:20 +0700

    add kitchern controller
    starting the kitchen

---
+++ Training170427/Controllers/KitchenController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Training170427.Controllers
+{
+    public class KitchenController : Controller
+    {
+        private RestaurantEntities db = new RestaurantEntities();
+        // GET: Kitchen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Training170427/Models/OrderItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Training170427.Models
+{
+    public class OrderItemViewModel
+    {
+        public int OrderID { get; set; }
+
+    }
+}
