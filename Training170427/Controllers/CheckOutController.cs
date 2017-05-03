fd4de21173e8f7270d800d3ae509438c4ea7ad5b
kevingautama <kevingautama97@gmail.com>
2017-05-03 15:37:09 +0700

    Kevin -

--- Training170427/Controllers/CheckOutController.cs
+++ Training170427/Controllers/CheckOutController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Training170427.Models;
 
 namespace Training170427.Controllers
 {
@@ -12,15 +13,40 @@ namespace Training170427.Controllers
         // GET: CheckOut
         public ActionResult Index()
         {
+            List<AddOrder> listdata = new List<AddOrder>();
             var data = (from a in db.Order
                        where a.Finish == false && a.IsDeleted != true
                        select a).ToList();
             
             foreach(var item in data)
             {
+                AddOrder order = new AddOrder();
+                var orderitem = (from a in db.OrderItem
+                                where a.OrderID == item.OrderID && a.IsDeleted != true && a.Status != "Cancel"
+                                select a.Status).ToList();
+                var status = 0;
+
+                foreach(var item2 in orderitem)
+                {
+                    if(item2 == "FinishCook")
+                    {
+                        status = status + 1;
+                    }
+                }
                 
+                if(status == orderitem.Count)
+                {
+                    order.OrderID = item.OrderID;
+                    order.TableName = (from a in db.Track
+                                       where a.OrderID == item.OrderID
+                                       select a.Table.TableName).FirstOrDefault();
+                    order.TypeName = item.Type.TypeName;
+
+                    listdata.Add(order);
+
+                }
             }
-            return View();
+            return View(listdata);
         }
     }
 }
