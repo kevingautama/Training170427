9e9f00c0d581aae6f5f9983093ddcdecc422c04c
kevingautama <kevingautama97@gmail.com>
2017-05-05 09:44:33 +0700

    Kevin

--- Training170427/Controllers/CheckOutController.cs
+++ Training170427/Controllers/CheckOutController.cs
@@ -48,5 +48,44 @@ namespace Training170427.Controllers
             }
             return View(listdata);
         }
+
+        public ActionResult CheckOutOrder(int id)
+        {
+            CheckOutModel data = new CheckOutModel();
+            var order = db.Order.Find(id);
+            if(order != null)
+            {
+                var tablename = (from a in db.Track
+                                 where a.OrderID == id
+                                 select a).FirstOrDefault();
+                var orderitem = (from a in db.OrderItem
+                                 where a.OrderID == id && a.Status == "FinishCook"
+                                 select new OrderItemViewModel
+                                 {
+                                     MenuID = a.MenuID,
+                                     MenuName = a.Menu.MenuName,
+                                     Qty = a.Qty,
+                                     
+
+                                 }).ToList();
+                double grandtotal = 0;
+                foreach(var item in orderitem)
+                {
+
+                    item.Price = Convert.ToString(item.Qty * Convert.ToDecimal(db.Menu.Find(item.MenuID).MenuPrice));
+                    grandtotal = grandtotal + Convert.ToDouble(item.Price);
+                }
+
+                data.OrderID = id;
+                data.TableName = tablename.Table.TableName;
+                data.GrandTotal = Convert.ToDecimal(grandtotal);
+                data.OrderItem = orderitem;
+                return View(data);
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
---
+++ Training170427/Models/CheckOutModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Training170427.Models
+{
+    public class CheckOutModel
+    {
+        public int OrderID { get; set; }
+        public string TableName { get; set; }
+        public List<OrderItemViewModel> OrderItem { get; set; }
+        public decimal GrandTotal { get; set; }
+
+    }
+}
